c4b96331218009314beb41cd35d782f9dbd118cb
dinitriana99 <dinitd25@gmail.com>
2019-10-04 09:44:49 +0700

    Dynamic Fragment

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/fragment/Pushup.java
@@ -0,0 +1,31 @@
+package polinema.ac.id.starterchapter05.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import polinema.ac.id.starterchapter05.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Pushup extends Fragment {
+
+
+    public Pushup() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pushup, container, false);
+    }
+
+}
